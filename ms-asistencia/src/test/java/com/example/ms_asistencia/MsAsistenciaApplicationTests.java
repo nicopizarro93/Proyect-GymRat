63a289e1e0bf6274606ddcfc4fc8fbc18cbad2e0
sebastian <sebastianreyes161@gmail.com>
2026-04-29 20:05:45 -0400

    Cree la carpeta asistencia

---
+++ ms-asistencia/src/test/java/com/example/ms_asistencia/MsAsistenciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ms_asistencia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAsistenciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
