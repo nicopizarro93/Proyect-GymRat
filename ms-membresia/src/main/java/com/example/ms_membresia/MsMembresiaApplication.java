585f1f912905bed57e72181c4d5c15b8be9a5b32
AO-Alumno <AO-Alumno@duoc.acad>
2026-04-29 20:05:42 -0400

    agregue membresia scws

---
+++ ms-membresia/src/main/java/com/example/ms_membresia/MsMembresiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ms_membresia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsMembresiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsMembresiaApplication.class, args);
+	}
+
+}
