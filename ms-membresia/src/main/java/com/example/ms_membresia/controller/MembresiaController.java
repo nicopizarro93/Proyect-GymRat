addaa6b42f9371a07bda7a118f4b774589dd5517
Sebastian Reyes <sebastianreyes161@gmail.com>
2026-05-14 01:01:31 -0400

    Solución de Request y mejoras

--- ms-membresia/src/main/java/com/example/ms_membresia/controller/MembresiaController.java
+++ ms-membresia/src/main/java/com/example/ms_membresia/controller/MembresiaController.java
@@ -1,14 +1,15 @@
 package com.example.ms_membresia.controller;
 
 import java.util.List;
+import java.util.Map; // <-- Importante para leer el JSON
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ms_membresia.model.MembresiaModel;
@@ -23,13 +24,15 @@ public class MembresiaController {
 
     private final MembresiaService membresiaService;
 
-    // 1. URL para contratar un nuevo plan
-    // Se usará así en Postman: POST localhost:8082/api/v1/membresias/contratar?rutAtleta=111-1&tipoPlan=Anual&mesesDuracion=12
+    // 1. URL para contratar un nuevo plan y darle permisos para ingresar al gym
+    // Se usará así en Thunder Client: POST localhost:8082/api/v1/membresias/contratar
     @PostMapping("/contratar")
-    public ResponseEntity<MembresiaModel> contratar(
-            @RequestParam String rutAtleta, 
-            @RequestParam String tipoPlan, 
-            @RequestParam int mesesDuracion) {
+    public ResponseEntity<MembresiaModel> contratar(@RequestBody Map<String, String> request) {
+        
+        // Sacamos los datos del JSON (del Map) uno por uno
+        String rutAtleta = request.get("rutAtleta");
+        String tipoPlan = request.get("tipoPlan");
+        int mesesDuracion = Integer.parseInt(request.get("mesesDuracion")); 
         
         MembresiaModel nuevaMembresia = membresiaService.contratarPlan(rutAtleta, tipoPlan, mesesDuracion);
         return new ResponseEntity<>(nuevaMembresia, HttpStatus.CREATED);
