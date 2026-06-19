aed583dd5dca86f705b14cd1c6b7561df92de907
Nicolas Pizarro <nico.pizarro93@gmail.com>
2026-06-19 12:10:05 -0400

    feat: pruebas unitarias v2

--- ms-atletas/src/main/java/com/gymrat/ms_atletas/dto/AtletaRequestDTO.java
+++ ms-atletas/src/main/java/com/gymrat/ms_atletas/dto/AtletaRequestDTO.java
@@ -1,8 +1,6 @@
 package com.gymrat.ms_atletas.dto;
 
 import com.gymrat.ms_atletas.model.RolEnum;
-
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -14,19 +12,15 @@ public class AtletaRequestDTO {
     
     @NotBlank(message = "El RUT es obligatorio")
     @Pattern(regexp = "^\\d{7,8}-[0-9Kk]$", message = "El RUT debe tener el formato 12345678-9 (sin puntos y con guion)")
-    @Schema(description = "RUT del atleta con guion", example = "12345678-9")
     private String rut;
 
-    @Schema(description = "Nombre del atleta", example = "juanito perez")
     @NotBlank(message = "El nombre es obligatorio")
     private String nombre;
 
-    @Schema(description = "Email del atleta", example = "juanito.perez@gymrat.cl")
-    @Email(message = "El email debe ser válido")
     @NotBlank(message = "El email es obligatorio")
+    @Email(message = "El email debe ser válido")
     private String email;
 
-    @Schema(description = "Rol del atleta",example = "MIEMBRO")
     @NotNull(message = "Debe asignar un rol al atleta")
     private RolEnum rol;
 }
